439609c246047957fba6ec5205fde27ac5dd95a9
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-25 22:29:07 +0530

    entities and services

--- src/main/java/com/vignesh/project/uber/uberApp/entities/User.java
+++ src/main/java/com/vignesh/project/uber/uberApp/entities/User.java
@@ -1,4 +1,29 @@
 package com.vignesh.project.uber.uberApp.entities;
 
+import com.vignesh.project.uber.uberApp.entities.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
 }
---
+++ src/main/java/com/vignesh/project/uber/uberApp/services/AuthService.java
@@ -0,0 +1,4 @@
+package com.vignesh.project.uber.uberApp.services;
+
+public interface AuthService {
+}
