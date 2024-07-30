871290b1f46e9bec175dd680c357f649efce7c5e
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-30 20:21:04 +0530

    auth implementation

--- src/main/java/com/vignesh/project/uber/uberApp/exceptions/RuntimeConflictException.java
+++ src/main/java/com/vignesh/project/uber/uberApp/exceptions/RuntimeConflictException.java
@@ -1,6 +1,10 @@
 package com.vignesh.project.uber.uberApp.exceptions;
 
 public class RuntimeConflictException extends RuntimeException {
+    public RuntimeConflictException() {
+    }
 
-
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/vignesh/project/uber/uberApp/services/impl/AuthServiceImpl.java
+++ src/main/java/com/vignesh/project/uber/uberApp/services/impl/AuthServiceImpl.java
@@ -3,9 +3,26 @@ package com.vignesh.project.uber.uberApp.services.impl;
 import com.vignesh.project.uber.uberApp.dto.DriverDto;
 import com.vignesh.project.uber.uberApp.dto.SignupDto;
 import com.vignesh.project.uber.uberApp.dto.UserDto;
+import com.vignesh.project.uber.uberApp.entities.User;
+import com.vignesh.project.uber.uberApp.entities.enums.Role;
+import com.vignesh.project.uber.uberApp.exceptions.RuntimeConflictException;
+import com.vignesh.project.uber.uberApp.repository.UserRepository;
 import com.vignesh.project.uber.uberApp.services.AuthService;
+import com.vignesh.project.uber.uberApp.services.RiderService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final RiderService riderService;
+
     @Override
     public String login(String email, String password) {
         return "";
@@ -13,7 +30,17 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public UserDto signup(SignupDto signupDto) {
-        return null;
+        User user = userRepository.findByEmail(signupDto.getEmail()).orElse(null);
+        if (user != null)
+            throw new RuntimeConflictException("Cannot signup, User already exists with email"+signupDto.getEmail());
+
+        User mappedUser = modelMapper.map(signupDto,User.class);
+        mappedUser.setRoles(Set.of(Role.RIDER));
+        User savedUser = userRepository.save(mappedUser);
+        riderService.createNewRider(savedUser);
+
+        return modelMapper.map(savedUser, UserDto.class);
+
     }
 
     @Override
