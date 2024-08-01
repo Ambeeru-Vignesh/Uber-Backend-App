b029458d0aca8c776793ebc27cf8fd7531b38dd7
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-01 21:54:38 +0530

    ride request service and driver controller

---
+++ src/main/java/com/vignesh/project/uber/uberApp/controllers/DriverController.java
@@ -0,0 +1,11 @@
+package com.vignesh.project.uber.uberApp.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/drivers")
+public class DriverController {
+}
--- src/main/java/com/vignesh/project/uber/uberApp/services/impl/RideRequestServiceImpl.java
+++ src/main/java/com/vignesh/project/uber/uberApp/services/impl/RideRequestServiceImpl.java
@@ -1,4 +1,28 @@
 package com.vignesh.project.uber.uberApp.services.impl;
 
-public class RideRequestServiceImpl {
+import com.vignesh.project.uber.uberApp.entities.RideRequest;
+import com.vignesh.project.uber.uberApp.exceptions.ResourceNotFoundException;
+import com.vignesh.project.uber.uberApp.repository.RideRequestRepository;
+import com.vignesh.project.uber.uberApp.services.RideRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RideRequestServiceImpl implements RideRequestService {
+
+    public final RideRequestRepository rideRequestRepository;
+
+    @Override
+    public RideRequest findRideRequestById(Long rideRequestId) {
+        return rideRequestRepository.findById(rideRequestId)
+                .orElseThrow(() -> new ResourceNotFoundException("RideRequest not found with id: " + rideRequestId));
+    }
+
+    @Override
+    public void update(RideRequest rideRequest) {
+        rideRequestRepository.findById(rideRequest.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("RideRequest not found with id: " + rideRequest.getId()));
+        rideRequestRepository.save(rideRequest);
+    }
 }
