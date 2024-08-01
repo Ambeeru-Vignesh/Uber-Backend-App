8a09853cae356be53f4bd87d75acd32681904d2d
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-01 22:15:37 +0530

    driver matching and calculation fare strategies

--- src/main/java/com/vignesh/project/uber/uberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
+++ src/main/java/com/vignesh/project/uber/uberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
@@ -2,13 +2,23 @@ package com.vignesh.project.uber.uberApp.strategies.impl;
 
 import com.vignesh.project.uber.uberApp.entities.Driver;
 import com.vignesh.project.uber.uberApp.entities.RideRequest;
+import com.vignesh.project.uber.uberApp.repository.DriverRepository;
 import com.vignesh.project.uber.uberApp.strategies.DriverMatchingStrategy;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class DriverMatchingHighestRatedDriverStrategy implements DriverMatchingStrategy {
+
+    private final DriverRepository driverRepository;
+
     @Override
-    public List<Driver> findMatchingDriver(RideRequest rideRequestDto) {
-        return List.of();
+    public List<Driver> findMatchingDriver(RideRequest rideRequest) {
+        return driverRepository.findTenNearbyTopRatedDrivers(rideRequest.getPickupLocation());
     }
 }
--- src/main/java/com/vignesh/project/uber/uberApp/strategies/impl/RideFareDefaultFareCalculationStrategy.java
+++ src/main/java/com/vignesh/project/uber/uberApp/strategies/impl/RideFareDefaultFareCalculationStrategy.java
@@ -2,12 +2,22 @@ package com.vignesh.project.uber.uberApp.strategies.impl;
 
 import com.vignesh.project.uber.uberApp.dto.RideRequestDto;
 import com.vignesh.project.uber.uberApp.entities.RideRequest;
+import com.vignesh.project.uber.uberApp.services.DistanceService;
 import com.vignesh.project.uber.uberApp.strategies.RideFareCalculationStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class RideFareDefaultFareCalculationStrategy implements RideFareCalculationStrategy {
+
+    private final DistanceService distanceService;
 
     @Override
     public double calculateFare(RideRequest rideRequest) {
-        return 0;
+
+        double distance = distanceService.calculateDistance(rideRequest.getPickupLocation(),
+                rideRequest.getDropOffLocation());
+        return distance*RIDE_FARE_MULTIPLIER;
     }
 }
