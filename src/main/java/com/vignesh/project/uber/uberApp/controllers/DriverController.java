6e4fae64945e183c8816fb610edb1d434544c4ec
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-01 22:33:05 +0530

    ride strategy manager and driver controller

--- src/main/java/com/vignesh/project/uber/uberApp/controllers/DriverController.java
+++ src/main/java/com/vignesh/project/uber/uberApp/controllers/DriverController.java
@@ -1,11 +1,31 @@
 package com.vignesh.project.uber.uberApp.controllers;
 
+import com.vignesh.project.uber.uberApp.dto.RideDto;
+import com.vignesh.project.uber.uberApp.dto.RideStartDto;
+import com.vignesh.project.uber.uberApp.repository.DriverRepository;
+import com.vignesh.project.uber.uberApp.services.DriverService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalTime;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/drivers")
 public class DriverController {
+
+    private final DriverService driverService;
+
+    @PostMapping("/acceptRide/{rideRequestId}")
+    public ResponseEntity<RideDto> acceptRide(@PathVariable Long rideRequestId){
+        return ResponseEntity.ok(driverService.acceptRide(rideRequestId));
+    }
+
+    @PostMapping("/startRide/{rideRequestId}")
+    public ResponseEntity<RideDto> startRide(@PathVariable Long rideRequestId,
+                                             @RequestBody RideStartDto rideStartDto) {
+        return ResponseEntity.ok(driverService.startRide(rideRequestId, rideStartDto.getOtp()));
+    }
+
 }
