26f3ee2d4c5200993e3d74f27ffbae57fded96c0
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-02 15:52:41 +0530

    ride and ride request service

--- src/main/java/com/vignesh/project/uber/uberApp/entities/Ride.java
+++ src/main/java/com/vignesh/project/uber/uberApp/entities/Ride.java
@@ -3,12 +3,20 @@ package com.vignesh.project.uber.uberApp.entities;
 import com.vignesh.project.uber.uberApp.entities.enums.PaymentMethod;
 import com.vignesh.project.uber.uberApp.entities.enums.RideStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ride {
 
     @Id
--- src/main/java/com/vignesh/project/uber/uberApp/services/impl/RiderServiceImpl.java
+++ src/main/java/com/vignesh/project/uber/uberApp/services/impl/RiderServiceImpl.java
@@ -8,12 +8,15 @@ import com.vignesh.project.uber.uberApp.entities.RideRequest;
 import com.vignesh.project.uber.uberApp.entities.Rider;
 import com.vignesh.project.uber.uberApp.entities.User;
 import com.vignesh.project.uber.uberApp.entities.enums.RideRequestStatus;
+import com.vignesh.project.uber.uberApp.exceptions.ResourceNotFoundException;
 import com.vignesh.project.uber.uberApp.repository.RideRequestRepository;
 import com.vignesh.project.uber.uberApp.repository.RiderRepository;
 import com.vignesh.project.uber.uberApp.services.RiderService;
 import com.vignesh.project.uber.uberApp.strategies.DriverMatchingStrategy;
 import com.vignesh.project.uber.uberApp.strategies.RideFareCalculationStrategy;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +24,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-
+@Slf4j
 public class RiderServiceImpl implements RiderService {
 
     private final ModelMapper modelMapper;
@@ -32,7 +35,9 @@ public class RiderServiceImpl implements RiderService {
 
 
     @Override
+    @Transactional
     public RideRequestDto requestRide(RideRequestDto rideRequestDto) {
+        Rider rider = getCurrentRider();
         RideRequest rideRequest = modelMapper.map(rideRequestDto, RideRequest.class);
         rideRequest.setRideRequestStatus(RideRequestStatus.PENDING);
 
@@ -75,5 +80,13 @@ public class RiderServiceImpl implements RiderService {
                 .build();
 
         return riderRepository.save(rider);
+    }
+
+    @Override
+    public Rider getCurrentRider() {
+
+        return riderRepository.findById(1L).orElseThrow(() -> new ResourceNotFoundException(
+                "Rider not found with id: " + 1
+        ));
     }
 }
