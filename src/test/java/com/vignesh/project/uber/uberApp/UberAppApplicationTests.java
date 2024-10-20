3ec6225ca71df6f888a371993fada9d52edb20d0
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-10-20 06:00:26 +0530

    application properties

--- src/test/java/com/vignesh/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/vignesh/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,29 @@
 package com.vignesh.project.uber.uberApp;
 
+import com.vignesh.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("brownboy353535@gmail.com","MY FIRST MAIL","HEY THE EMAIL SENT SUCCESSFULLY");
+	}
+
+	@Test
+	void sendEmailMultiple() {
+		String[] emails = {
+				"vilis40086@adosnan.com",
+				"brownboy353535@gmail.com"
+		};
+		emailSenderService.sendEmail(emails,"MY FIRST MAIL","HEY THE EMAIL SENT SUCCESSFULLY");
+
 	}
 
 }
