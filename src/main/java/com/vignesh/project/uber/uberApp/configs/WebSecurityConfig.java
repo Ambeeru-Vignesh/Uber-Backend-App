898118fd81ceda224ad5839d530a3de9ee467cff
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-12-21 01:55:41 +0530

    few fixes

--- src/main/java/com/vignesh/project/uber/uberApp/configs/WebSecurityConfig.java
+++ src/main/java/com/vignesh/project/uber/uberApp/configs/WebSecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
-    private static final String[] PUBLIC_ROUTES ={"/auth/**"};
+    private static final String[] PUBLIC_ROUTES ={"/","/auth/**","/v3/api-docs/**","/actuator/**","/swagger-ui/**"};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
